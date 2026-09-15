5821f5efab3346f5592544465ef2ea0e4d9c7451
maximilianorojas2705-lumi <288036807+maximilianorojas2705-lumi@users.noreply.github.com>
2026-09-15 17:56:12 -0300

    Change model from llama-3.3 to llama-3.1

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def telegram():
                 from groq import Groq
                 client = Groq(api_key=GROQ_KEY)
                 completion = client.chat.completions.create(
-                    model="llama-3.3-70b-versatile",
+                    model="llama-3.1-8b-instant",
                     messages=[{"role":"user","content": text}]
                 )
                 reply = completion.choices[0].message.content
